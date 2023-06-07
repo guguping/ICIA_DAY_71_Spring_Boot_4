71eab632e4ac392b858d072bcfd5e283625a5b24
guguping <qkfhandur@naver.com>
2023-06-07 15:36:08 +0900

    파일첨부

--- src/main/java/com/example/board/dto/BoardDTO.java
+++ src/main/java/com/example/board/dto/BoardDTO.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -42,10 +43,16 @@ public class BoardDTO {
         //파일 여부에 따른 코드 추가
         if (boardEntity.getFileAttached() == 1){
             boardDTO.setFileAttached(1);
-            for (BoardFileEntity board : boardEntity.getBoardFileEntityList()){
-                boardDTO.setOriginalFileName(Collections.singletonList(board.getOriginalFileName()));
-                boardDTO.setStoredFileName(Collections.singletonList(board.getStoredFileName()));
+            // 파일 이름을 담을 리스트 객체 선언
+            List<String> originalFileNameList = new ArrayList<>();
+            List<String> storedFileNameList= new ArrayList<>();
+            //첨부파일에 각각 접근
+            for (BoardFileEntity boardFileEntity : boardEntity.getBoardFileEntityList()){
+                originalFileNameList.add(boardFileEntity.getOriginalFileName());
+                storedFileNameList.add(boardFileEntity.getStoredFileName());
             }
+            boardDTO.setOriginalFileName(originalFileNameList);
+            boardDTO.setStoredFileName(storedFileNameList);
         } else {
             boardDTO.setFileAttached(0);
         }
